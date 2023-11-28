f68ceba341bb0fc5f350ec18ec97cca5bb2014b9
awlsring <awlsring@gmail.com>
2023-11-27 22:35:20 -0500

    refactor: identifier less restrictive

--- apps/local/internal/core/domain/machine/machine_identifier.go
+++ apps/local/internal/core/domain/machine/machine_identifier.go
@@ -1,34 +1,9 @@
 package machine
 
-import (
-	"errors"
-	"regexp"
-
-	camperror "github.com/awlsring/camp/internal/pkg/errors"
-)
-
-const (
-	MachineIdentifierPattern = "^m-"
-	MachineIdentifierLength  = 36
-)
-
-var (
-	ErrInvalidMachineIdentifier = errors.New("invalid machine identifier")
-)
-
 type Identifier string
 
 func IdentifierFromString(identifier string) (Identifier, error) {
-	re := regexp.MustCompile(MachineIdentifierPattern)
-	if !re.MatchString(identifier) {
-		return "", ErrInvalidMachineIdentifier
-	}
-
-	if len(identifier) != MachineIdentifierLength {
-		return "", camperror.New(camperror.ErrValidation, ErrInvalidMachineIdentifier)
-	}
-
-	return Identifier(identifier), nil
+	return NonNullString[Identifier](identifier)
 }
 
 func (m Identifier) String() string {
